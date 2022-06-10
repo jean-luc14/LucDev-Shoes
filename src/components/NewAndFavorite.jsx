2fe4fa26cf216ccb7e1824c5be4a58a5726ece78
jean luc <eyrambuckner6@gmail.com>
2022-06-10 16:55:20 +0000

    perf:add progress bar to new and favorite products slider

--- src/components/NewAndFavorite.jsx
+++ src/components/NewAndFavorite.jsx
@@ -12,14 +12,43 @@ const NewAndFavorite = (props) => {
 
   useEffect(() => {
     //Full Slider
-    const sliderCtn = document.querySelectorAll(".slideshow-container");
+    const sliderCtn = document.querySelectorAll(
+      ".favorite_and_new_shoes_slider .slideshow-container"
+    );  
+    
     let holding = false;
     let firstClickX;
     let alreadyScrl;
     let velocity;
-    let rafID;
+    let rafID; 
 
     sliderCtn.forEach((Ctn) => {
+      let progressWidth;
+      let progressClickWidth;
+      let progressLeft;
+      let progressClickLeft;
+
+      let progressBarClick = Ctn.parentNode.childNodes[1];
+      let progressBar = Ctn.parentNode.childNodes[1].childNodes[0];
+
+      let totalWidth = Ctn.childNodes[0].getBoundingClientRect().width;
+
+      progressWidth = (Ctn.getBoundingClientRect().width / totalWidth) * 100;
+      progressClickWidth = progressBarClick.getBoundingClientRect().width
+      progressClickLeft = progressBarClick.getBoundingClientRect().left;
+
+      progressBar.style.width = `${progressWidth}%`;  
+
+      progressBarClick.addEventListener("click", (e) => {
+        holding = false;
+        let x = e.clientX - progressClickLeft;
+        let newScrollLeft = (x / progressClickWidth) * totalWidth;
+        Ctn.scrollLeft = newScrollLeft;
+
+        animateProgressBar();
+        stopTransition();
+      });
+
       Ctn.addEventListener("mousedown", (e) => {
         holding = true;
         firstClickX = e.pageX - Ctn.offsetLeft;
@@ -33,7 +62,9 @@ const NewAndFavorite = (props) => {
         const scrolled = x - firstClickX;
         const prevScrollLeft = Ctn.scrollLeft;
         Ctn.scrollLeft = alreadyScrl - scrolled;
-        velocity = Ctn.scrollLeft - prevScrollLeft;
+        velocity = Ctn.scrollLeft - prevScrollLeft; 
+
+        animateProgressBar();
       });
 
       Ctn.addEventListener("mouseup", () => {
@@ -43,25 +74,37 @@ const NewAndFavorite = (props) => {
 
       Ctn.addEventListener("mouseleave", () => {
         holding = false;
-      });
+      }); 
 
+      //animate progress bar of slider
+      const animateProgressBar = () => {
+        progressLeft = (Ctn.scrollLeft / totalWidth) * 100;
+        progressBar.style.left = `${progressLeft}%`;
+      }
+      
+      //start transition animation of slider
       const startTransition = () => {
         stopTransition();
         rafID = requestAnimationFrame(decreasingTransition);
       };
 
+      //stop transition animation of slider
       const stopTransition = () => {
         cancelAnimationFrame(rafID);
       };
 
+      //animate slider with a slowing down
       const decreasingTransition = () => {
-        Ctn.scrollLeft += velocity;
+        Ctn.scrollLeft += velocity; 
+        animateProgressBar()
+
         velocity *= 0.95;
         if (Math.abs(velocity) > 0.5) {
           rafID = requestAnimationFrame(decreasingTransition);
         }
       };
 
+     // mobile events 
       Ctn.addEventListener("touchstart", (e) => {
         holding = true;
         firstClickX = e.targetTouches[0].pageX - Ctn.offsetLeft;
@@ -81,8 +124,11 @@ const NewAndFavorite = (props) => {
         const prevScrollLeft = Ctn.scrollLeft;
         Ctn.scrollLeft = alreadyScrl - scrolled;
         velocity = Ctn.scrollLeft - prevScrollLeft;
+
+        animateProgressBar();
       });
-    });
+    }); 
+ 
   }, []);
 
   return (
@@ -92,23 +138,33 @@ const NewAndFavorite = (props) => {
       }`}
     >
       <h2>New Arrivals</h2>
-      <section className="slideshow-container">
-        <div className="slideshow">
-          {newProducts.map((e, index) => (
-            <div className="child" key={index}>
-              <ProductCard productProps={e} newAndFavorite={true} />
-            </div>
-          ))}
+      <section> 
+        <div className="slideshow-container">
+          <div className="slideshow">
+            {newProducts.map((e, index) => (
+              <div className="child" key={index}>
+                <ProductCard productProps={e} newAndFavorite={true} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="click_slider_bar">
+          <div className="slider_bar"></div>
         </div>
       </section>
       <h2>Our Favorites</h2>
-      <section className="slideshow-container">
-        <div className="slideshow">
-          {favoriteProducts.map((e, index) => (
-            <div className="child" key={index}>
-              <ProductCard productProps={e} newAndFavorite={true} />
-            </div>
-          ))}
+      <section> 
+        <div className="slideshow-container">
+          <div className="slideshow">
+            {favoriteProducts.map((e, index) => (
+              <div className="child" key={index}>
+                <ProductCard productProps={e} newAndFavorite={true} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="click_slider_bar">
+          <div className="slider_bar"></div>
         </div>
       </section>
     </div>
