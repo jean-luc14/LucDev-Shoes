1788f6ef95ef0fea363914ed5ca42cc3db42f857
jean luc <eyrambuckner6@gmail.com>
2022-06-12 22:17:30 +0000

    set basename of project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import './scss/index.scss';
-import {store} from './redux/Store'
-import {Provider} from 'react-redux'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./scss/index.scss";
+import { store } from "./redux/Store";
+import { Provider } from "react-redux";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 //basename="/LucDev-Shoes"
 
 ReactDOM.render(
-  <BrowserRouter >
+  <BrowserRouter basename="/LucDev-Shoes">
     <Provider store={store}>
       <App />
     </Provider>
