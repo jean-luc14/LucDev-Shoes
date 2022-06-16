d0ad9451b1f65b28bd16962776153e83c520e187
Luc Buckner <lucDevBuckner09@gmail.com>
2022-06-16 12:07:05 +0000

    feat(navbar):put focus on search input if search input is open or active

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,109 +1,107 @@
-import React,{ useState,useEffect} from 'react'
-import {useNavigate} from 'react-router-dom'
-import Search_icon from '../Assets/icons/search_icon.png';
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import Search_icon from "../Assets/icons/search_icon.png";
 import { searchProducts } from "../Assets/data/ProductData";
-import { useSelector,useDispatch } from 'react-redux'
-import {set} from '../redux/search/ActiveSearchSlice'
+import { useSelector, useDispatch } from "react-redux";
+import { set } from "../redux/search/ActiveSearchSlice";
 
+const Search = () => {
+  const [dynamic_search_data, setDynamic_search_data] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-const Search = () => { 
-  const [dynamic_search_data,setDynamic_search_data] = useState([])
-  const [inputValue, setInputValue] = useState('');
+  const activeSearch = useSelector((state) => state.search.value);
 
-  const activeSearch = useSelector(state => state.search.value)
-
+  const inputElement = useRef(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   //put input value to state
-  const searchProductFoo = e => {
+  const searchProductFoo = (e) => {
     setInputValue(e.target.value);
-  }
-  
- //update the value of search in redux store to active search input and oder
+  };
+
+  //update the value of search in redux store to active search input and oder
   const animSearchFoo = () => {
     dispatch(set(!activeSearch));
-  }
+    inputElement.current.focus();
+  };
 
-// go to product page 
-  const goToProductPage = (catalogSlug,id) => {
-    navigate(`/catalog=${catalogSlug}&id=${id}`)
-    setInputValue('')
+  // go to product page
+  const goToProductPage = (catalogSlug, id) => {
+    navigate(`/catalog=${catalogSlug}&id=${id}`);
+    setInputValue("");
     animSearchFoo();
-  }
+  };
 
   //Submit
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate(`/search=${inputValue}`);
-    setInputValue('');
+    setInputValue("");
     animSearchFoo();
-  }
+  };
   useEffect(() => {
     // search products and put it in state
     var dynamic_result = searchProducts(inputValue);
     setDynamic_search_data(dynamic_result);
   }, [inputValue]);
-  
+
   return (
-    <div className={`search_wrapper ${activeSearch ? "animSearch" : ""}`}> 
+    <div className={`search_wrapper ${activeSearch ? "animSearch" : ""}`}>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Search"
-          className='search_input'
+          className="search_input"
           value={inputValue}
           onInput={searchProductFoo}
+          ref={inputElement}
         ></input>
         <img
           className="search_icon"
           src={Search_icon}
           onClick={animSearchFoo}
         />
-        {(inputValue.length > 2) & activeSearch ?
-          (
-            <>
-              <div className="dynamic_search_results">
-                <span
-                  className={`${ 
-                    (dynamic_search_data.length > 0)  
-                      ? "triangle"
-                      : ""
-                  }`}
-                ></span>
-                { dynamic_search_data.map((e, i) => (
-                    <>
-                      <div
-                        key={i}
-                        className="dynamic_search_item"
-                        onClick={() => goToProductPage(e.catalogSlug, e.id)}
-                      >
-                        <div className="dynamic_search_item_img">
-                          <img src={e.color[0].img} alt={e.name} />
-                        </div>
-                        <div className="dynamic_search_item_content">
-                          <p className="dynamic_search_item_content_price">
-                            US ${e.price}
-                          </p>
-                          <p className="dynamic_search_item_content_category">
-                            {e.catalogSlug}
-                          </p>
-                          <p className="dynamic_search_item_content_name">
-                            {e.name}
-                          </p>
-                        </div>
-                      </div>
-                   </>
-                ))}
-                { (dynamic_search_data.length > 0) ? (
+        {(inputValue.length > 2) & activeSearch ? (
+          <>
+            <div className="dynamic_search_results">
+              <span
+                className={`${
+                  dynamic_search_data.length > 0 ? "triangle" : ""
+                }`}
+              ></span>
+              {dynamic_search_data.map((e, i) => (
+                <>
+                  <div
+                    key={i}
+                    className="dynamic_search_item"
+                    onClick={() => goToProductPage(e.catalogSlug, e.id)}
+                  >
+                    <div className="dynamic_search_item_img">
+                      <img src={e.color[0].img} alt={e.name} />
+                    </div>
+                    <div className="dynamic_search_item_content">
+                      <p className="dynamic_search_item_content_price">
+                        US ${e.price}
+                      </p>
+                      <p className="dynamic_search_item_content_category">
+                        {e.catalogSlug}
+                      </p>
+                      <p className="dynamic_search_item_content_name">
+                        {e.name}
+                      </p>
+                    </div>
+                  </div>
+                </>
+              ))}
+              {dynamic_search_data.length > 0 ? (
                 <input type="submit" value="Plus"></input>
-                ) : null}
-              </div>
-            </>
-          )
-        :null }
+              ) : null}
+            </div>
+          </>
+        ) : null}
       </form>
     </div>
   );
-}
-export default Search
+};
+export default Search;
