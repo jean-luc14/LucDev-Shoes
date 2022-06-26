430f5bed4fd123fb47e45c693badb336f48bca1c
Luc Buckner <lucDevBuckner09@gmail.com>
2022-06-26 01:27:45 +0000

    feat(product view modal):receive from firestore the product requested by the user, and display it in product view modal

--- src/components/ProductViewModal.jsx
+++ src/components/ProductViewModal.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import ProductView from "./ProductView";
 import { getProductByCategoryAndId } from "../Assets/data/ProductData";
 import { useSelector } from "react-redux";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase-config";
 
 const ProductViewModal = () => {
   const productId = useSelector((state) => state.productModal.value.id);
@@ -11,7 +13,27 @@ const ProductViewModal = () => {
   const [product, setProduct] = useState(undefined);
 
   useEffect(() => {
-    setProduct(getProductByCategoryAndId(productCategory, productId));
+    //get from firestore product which have as category param.category, id param.id and put it to the state
+    const getProductByCategoryAndId = async () => {
+      if (productCategory && productId) {
+        let q = query(
+          collection(db, "productData"),
+          where("category", "==", productCategory),
+          where("id", "==", productId)
+        );
+
+        const querySnapshot = await getDocs(q);
+        let prod;
+        querySnapshot.forEach((doc) => {
+          prod = doc.data();
+        });
+
+        setProduct(prod);
+      } else {
+        setProduct(undefined);
+      }
+    };
+    getProductByCategoryAndId();
   }, [productCategory, productId]);
 
   return (
